ab9a0e22f6bf5c98c9d2d3c1945c9c9f21356757
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2018-10-23 18:41:29 +0100

    tabs/spaces

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -33,8 +33,8 @@ async def on_message(message):
     #a.greetings(message)
     	
 	for i in greetings:	
-	        if i in message.content:
-		        msg = greetings[random.randint(0, len(greetings)-1)] + ' {0.author.mention}'.format(message)
+		if i in message.content:
+			msg = greetings[random.randint(0, len(greetings)-1)] + ' {0.author.mention}'.format(message)
 			await client.send_message(message.channel, msg)
 
 	if message.content.startswith('!URL'):
