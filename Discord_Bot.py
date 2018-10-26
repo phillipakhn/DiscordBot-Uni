44d10e0fa5d7f4533bc94166ac0cadfab4f246f1
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2018-10-26 01:51:09 +0100

    Library Tenplate

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -18,115 +18,43 @@ client = discord.Client()
 #print("502223039912476692".get_channel())
 
 @client.event
+async def alastair(message):
+	if message.author == client.user:
+		return
+#	if not lstmsg:
+#		lstmsg = ""
+	#a.dab(message)
+	msg = ""
+	msg = msg + str(a.human(message))
+	msg = msg + str(a.greetings(message))
+	msg = msg + str(a.url(message))
+	msg = msg + str(a.test(message))
+	msg = msg + str(a.update(message))
+	msg = msg + str(a.exitBot(message))
+	msg = msg + str(a.code(message))
+	msg = msg + str(a.gitHelp(message))
+	msg = msg.strip('None')
+	print("MESSAGE" + msg)
+	#if not msg:
+	await client.send_message(message.channel, msg)
+	
+
+@client.event
 async def on_message(message):
 	
 	global lstmsg
-	
-	with open("greetings.txt",'rb') as rfp:
-		greetings = pickle.load(rfp)
-	with open("url.txt",'rb') as rfp:
-		url = pickle.load(rfp)
 
 	print(message.content)
-
-	if message.author == client.user:
-		return 
 		
-	if message.content.startswith('!Human'):
-		return
-		
-	if message.content.startswith('!Show'):
-		msg = 'This is working'
-		await client.send_message(message.channel, msg)
-		return
+	await alastair(message)
+	#await kieran
+	#await 
 
-    #a.greetings(message)
-    	
-	for i in greetings:	
-		if i in message.content:
-			msg = greetings[random.randint(0, len(greetings)-1)] + ' {0.author.mention}'.format(message)
-			await client.send_message(message.channel, msg)
-			return
 
-	if message.content.startswith('!URL'):
-		i = 0
-		while i <= (len(url)-1):	
-			msg = url[i]
-			await client.send_message(message.channel, msg)	
-			i = i + 1
-		return
-	
-	
-			
-	if message.content.startswith('!Code'):
-		C = open("/home/pi/DiscordBot/Discord_Bot.py", "r")
-		msg = ('{0.author.mention}'.format(message) + " - Here is the code: \n" + C.read())
-		await client.send_message(message.channel, msg)
-		C.close()
-		
-	if message.content.startswith('!Exit'):
-		os.system('python3 StartPi.py')
-		exit()
-		
-	if message.content.startswith('!Update'):
-		os.system('cd ~ \n ./update.sh')
-		await client.send_message(message.channel, "Git Updated")
-		exit()
 
-	if message.content.startswith('!dab4eva'):
-		while True:
-			msg = 'Dab'
-			await client.send_message(message.channel, msg)
-	if message.content.startswith('!help'):
-		msg = '---CLONE GIT---'
-		await client.send_message(message.channel, msg)
-		msg = '1) git clone https://github.coventry.ac.uk/hollan84/DiscordBot'
-		await client.send_message(message.channel, msg)
-		msg = '2) (Put in Username and Password) \n'
-		await client.send_message(message.channel, msg)
-		msg = '---PUSH GIT---'
-		await client.send_message(message.channel, msg)
-		msg = '1) cd DiscordBot'
-		await client.send_message(message.channel, msg)	
-		msg = '2) git init'
-		await client.send_message(message.channel, msg)	
-		msg = '3) git add .'
-		await client.send_message(message.channel, msg)
-		msg = "4) git commit -m 'Discord'"
-		await client.send_message(message.channel, msg)	
-		msg = '5) git remote add origin https://github.coventry.ac.uk/hollan84/DiscordBot'
-		await client.send_message(message.channel, msg)		
-		msg = '6) git push -u origin master'
-		await client.send_message(message.channel, msg)
-		msg = '7) (Put in Username and Password)'
-		await client.send_message(message.channel, msg)
-		msg = '---PULL GIT---'
-		await client.send_message(message.channel, msg)
-		msg = '1) git pull'
-		await client.send_message(message.channel, msg)
-		msg = '2) (Put in Username and Password) \n'
-		await client.send_message(message.channel, msg)
-		return
-	
-	if message.content.startswith('!addurl'):
-		#global lstmsg
-		url.append(lstmsg)
-		print(url)
-		pickle.dump(url, open("url.txt",'wb'))
-		return
-	if message.content.startswith('!greeting'):
-		greetings.append(lstmsg)
-		print(greetings)
-		pickle.dump(greetings, open("greetings.txt",'wb') )
-		return
-	else:
-		lstmsg = message.content
-		msg = "I can't find that within my memory, type a category."
-		await client.send_message(message.channel, msg)
-		msg = "Categories:"
-		await client.send_message(message.channel, msg)
-		msg = "!greeting, !addurl"
-		await client.send_message(message.channel, msg)	
+
+
+
 
 @client.event
 async def on_ready():
--- alastair.py
+++ alastair.py
@@ -1,11 +1,94 @@
-import discord, logging, time, random, pickle
+import discord, time, random, pickle, os
 
 TOKEN = 'NTAyMjI2MDYzNzYyMzkxMDUw.Dqk38g.KFDtyaHwMfRNK6RRUKYfpDj9L9Y'
 client = discord.Client()
 
-async def greetings(message, greetings):
+def human(message):
+	if message.content.startswith('!Human'):
+		return
+			
+def greetings(message):
+	with open("greetings.txt",'rb') as rfp:
+		greetings = pickle.load(rfp)
 	for i in greetings:	
 		if i in message.content:
 			msg = greetings[random.randint(0, len(greetings)-1)] + ' {0.author.mention}'.format(message)
-			client.send_message(message.channel, msg)
-			return
+			return msg
+			
+def url(message):
+	if message.content.startswith('!URL'):
+		i = 0
+		with open("url.txt",'rb') as rfp:
+			url = pickle.load(rfp)
+		msg = ""
+		while i <= (len(url)-1):	
+			msg = msg + "\n" + url[i]
+			i = i + 1
+		return msg
+			
+def test(message):
+	if message.content.startswith('!Test'):
+		msg = 'This is working'
+		return msg
+		
+def update(message):
+	if message.content.startswith('!Update'):
+		os.system('cd ~ \n ./update.sh')
+		exit()
+		
+def exitBot(message):
+	if message.content.startswith('!Exit'):
+		exit()
+		
+def add(lstmsg, message):
+	if message.content.startswith('!addurl'):
+		#global lstmsg
+		url.append(lstmsg)
+		print(url)
+		pickle.dump(url, open("url.txt",'wb'))
+		return
+	if message.content.startswith('!greeting'):
+		greetings.append(lstmsg)
+		print(greetings)
+		pickle.dump(greetings, open("greetings.txt",'wb') )
+		return
+		
+def code(message):
+	if message.content.startswith('!Code'):
+		C = open("/home/pi/DiscordBot/Discord_Bot.py", "r")
+		msg = ('{0.author.mention}'.format(message) + " - Here is the code: \n" + C.read())
+		C.close()
+		return msg
+		
+#def dab(message):
+#	if message.content.startswith('!dab4eva'):
+#		while True:
+#			msg = 'Dab'
+#			await client.send_message(message.channel, msg)
+		
+def notInMem(message):
+	lstmsg = message.content
+	msg = "I can't find that within my memory, type a category. \n Categories: \n !greeting, !addurl"
+	return msg
+	
+def gitHelp(message):
+	if message.content.startswith('!help'):
+		msg = '---CLONE GIT--- \n'
+		msg = msg + '1) git clone https://github.coventry.ac.uk/hollan84/DiscordBot \n'
+		msg = msg + '2) (Put in Username and Password) \n'
+		msg = msg + '---PUSH GIT--- \n'
+		msg = msg + '1) cd DiscordBot \n'
+		msg = msg + '2) git init \n'
+		msg = msg + '3) git add . \n'
+		msg = msg + "4) git commit -m 'Discord' \n"
+		msg = msg + '5) git remote add origin https://github.coventry.ac.uk/hollan84/DiscordBot \n'	
+		msg = msg + '6) git push -u origin master \n'
+		msg = msg + '7) (Put in Username and Password) \n'
+		msg = msg + '---PULL GIT--- \n'
+		msg = msg + '1) git pull \n'
+		msg = msg + '2) (Put in Username and Password) \n'
+		return msg
+			
+def testInput(message):
+	print(message)
+	
