35af6045d91a07d15a239ccc0cf540d854b61a97
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2018-10-29 20:13:46 +0000

    Update alastair.py

--- alastair.py
+++ alastair.py
@@ -60,6 +60,7 @@ def temperature(message):
 		page = requests.get('http://100.90.93.150')
 		tree = html.fromstring(page.content)
 		t = (tree.xpath('//h1/text()'))
+		t = str(t)
 		t = t.strip("[' ', '")
 		t = t.strip('Â')
 		t = t.strip("', ' ']")
