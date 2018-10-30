d04e9543fc3e2e7820187f26502d7bc88fcd5f6f
Kieran Dhir (dhirk) <dhirk@coventry.ac.uk>
2018-10-30 12:04:07 +0000

    Update Kieran.py

--- Kieran.py
+++ Kieran.py
@@ -1,49 +1,16 @@
 import discord, logging, time, random, pickle, os
-import alastair as a
-#import karl as k
-#import tomas as t
-#import mateusz as m
-#import mango as mngo
-from Kieran import GoogleEarth as e
 TOKEN = 'NTAyMjI2MDYzNzYyMzkxMDUw.Dqk38g.KFDtyaHwMfRNK6RRUKYfpDj9L9Y'
 
-global greetings
-greetings = ["Hello", "Hi", "Yo"]
-url = ["https://github.coventry.ac.uk/hollan84/DiscordBot"]
-
-#Update test
-
-#print(greetings)
-
-#pickle.dump(url, open("url.txt",'wb') )
-#pickle.dump(greetings, open("greetings.txt",'wb') )
 
 client = discord.Client()
 
-#print("502223039912476692".get_channel())
-
 @client.event
-async def alastair(message):
+async def kieran(message):
 	if message.author == client.user:
 		return
-#	if not lstmsg:
-#		lstmsg = ""
-	#a.dab(message)
 	msg = ""
-	msg = msg + str(a.human(message))
-	msg = msg + str(a.greetings(message))
-	msg = msg + str(a.url(message))
-	msg = msg + str(a.test(message))
-	msg = msg + str(a.update(message))
-	msg = msg + str(a.exitBot(message))
-	msg = msg + str(a.code(message))
-	msg = msg + str(a.gitHelp(message))
-	msg = msg + str(a.temperature(message))
 	msg = msg + str(e.Google(message))
-	msg = msg.strip('None')
-	print("MESSAGE" + msg)
-	#if not msg:
-	await client.send_message(message.channel, msg)
+	await client.send_message(message.channel, msg) # send message to discord
 	
 
 @client.event
@@ -53,14 +20,7 @@ async def on_message(message):
 
 	print(message.content)
 		
-	await alastair(message)
-	#await kieran
-	#await 
-
-
-
-
-
+	await kieran(message)
 
 
 @client.event
