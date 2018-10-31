8b34369c49219be72658e7e081ffff98bdd57b72
hollan84 <hollan84@coventry.ac.uk>
2018-10-31 11:58:33 +0000

    Fixed Karl Prime

--- Prime.py
+++ Prime.py
@@ -7,19 +7,25 @@ client = discord.Client()
 @client.event
 async def primeN(message):
     if message.content.startswith('!Primes'):
-        if (message==1):
+        msg = str(message.content)
+        msg = msg.strip('!Primes ')
+        msg = int(msg)
+        print(msg)
+        if (msg==1):
+            await client.send_message(message.channel, "This is not a prime number.")
             return False
-    elif (message==2):
-        return True
-    else:
-        for x in range(2,message):
-            if(message % x==0):
-                print("This is not a prime number.")
-                await client.send_message(message.channel, "This is not a prime number.")
-                return False 
-        print("This is a prime number.") 
-        await client.send_message(message.channel, "This is a prime number.")
-        return True 
+        elif (msg==2):
+            await client.send_message(message.channel, "This is a prime number.")
+            return True
+        else:
+            for x in range(2,msg):
+                if(msg%x == 0):
+                    print("This is not a prime number.")
+                    await client.send_message(message.channel, "This is not a prime number.")
+                    return False 
+            print("This is a prime number.") 
+            await client.send_message(message.channel, "This is a prime number.")
+            return True 
     
 @client.event
 async def on_message(message):
