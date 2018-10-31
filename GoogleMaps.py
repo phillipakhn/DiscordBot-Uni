3300b944af9450e94d43a4048863fd5da50249d0
Kieran Dhir (dhirk) <dhirk@coventry.ac.uk>
2018-10-31 11:19:25 +0000

    Version 2

---
+++ GoogleMaps.py
@@ -0,0 +1,29 @@
+# Google Earth
+import webbrowser
+import discord
+import WebSite as w
+
+def Google(message):
+		Postcode = str(message.content) # converting the discord message to a string
+		Postcode = Postcode.replace("!GoogleMaps","")
+		Postcode = Postcode.replace(" ","")
+		Valid = Validate(Postcode)
+		if Valid == False:
+			msg = "That postcode is impossible!"
+			return msg
+		else:
+			w.Maps(Postcode)
+			msg = "Here you go "
+			return msg	
+		
+def Validate(Postcode):
+	length = len(Postcode)		
+	if length > 7 or length < 5:
+		return False
+		print("Invalid postcode")		
+	elif Postcode[0].isalpha() == False:
+		return False
+	elif Postcode[length-1].isalpha() == False: # must be length-1 becuase length does'nt include index 0
+		return False
+	else:
+		return True	 	
--- Kieran.py
+++ Kieran.py
@@ -1,5 +1,5 @@
-import discord, logging, time, random, pickle, os
-import GoogleEarth as e
+import discord
+import GoogleMaps as e
 
 TOKEN = 'NTAyMjI2MDYzNzYyMzkxMDUw.Dqk38g.KFDtyaHwMfRNK6RRUKYfpDj9L9Y'
 
@@ -11,15 +11,16 @@ async def kieran(message):
 	if message.author == client.user:
 		return
 	msg = ""
-	msg = msg + str(e.Google(message))
+	if message.content.startswith('!GoogleMaps'):
+		msg = msg + str(e.Google(message))
+	if not msg:
+		return
 	msg = msg.strip('None')
 	await client.send_message(message.channel, msg) # send message to discord
 	
 
 @client.event
 async def on_message(message):
-	
-	global lstmsg
 
 	print(message.content)
 		
