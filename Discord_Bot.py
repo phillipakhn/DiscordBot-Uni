3cc460f7bdb2fb358014a3fe9ee134ffb67cc859
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2018-11-12 21:08:36 +0000

    Update Discord_Bot.py

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -98,6 +98,7 @@ async def on_message(message):
 @client.event
 async def on_ready():
 	print('Logged in as ' + client.user.name)
+	msg = "Bot Started at " + startup " on " + get('https://api.ipify.org').text
 	await client.send_message(client.get_channel('502223039912476694'), "Bot Started")
 
 client.run(TOKEN)
