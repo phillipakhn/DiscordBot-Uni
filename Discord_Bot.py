972a2262f2d6335b296fc50d859e94f333999d20
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2018-11-12 20:53:05 +0000

    Update Discord_Bot.py

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -57,6 +57,8 @@ async def modules(message):
 	msg = msg + str(a.add(oldmsg, message))
 	msg = msg.strip('None')
 	#print("MESSAGE" + msg)
+	if msg == "RETURN":
+		return
 	if not msg:
 		msg = str(a.notInMem(message))
 	if message.content.startswith('!BotInfo'):
