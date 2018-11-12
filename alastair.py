4a612b744f5115efaf373b4d486321ed13911da3
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2018-11-12 13:27:40 +0000

    Update alastair.py

--- alastair.py
+++ alastair.py
@@ -13,6 +13,8 @@ def greetings(message):
 	for i in greetings:	
 		if i in message.content:
 			msg = greetings[random.randint(0, len(greetings)-1)] + ' {0.author.mention}'.format(message)
+			if 70 < random.randint(0, 100, 1):
+				msg = msg + "\n " + fQuestion("How are you?")
 			return msg
 			
 def fQuestion(message):
