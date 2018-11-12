90d81594a0404bcf9e44165d117d726716c9a86a
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2018-11-12 21:03:14 +0000

    Update Discord_Bot.py

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -98,6 +98,6 @@ async def on_message(message):
 @client.event
 async def on_ready():
 	print('Logged in as ' + client.user.name)
-	await client.send_message("502223039912476694", "Bot Started")
+	await client.send_message(502223039912476694, "Bot Started")
 
 client.run(TOKEN)
