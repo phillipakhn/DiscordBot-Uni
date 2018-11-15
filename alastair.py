c8db3fc0e2fe4fb600ea9c4bc24d59d8a74d4402
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2018-11-15 17:02:35 +0000

    Update alastair.py

--- alastair.py
+++ alastair.py
@@ -158,12 +158,11 @@ def temperature(message):
 		url = 'http://100.90.93.150/source'
 		with urllib.request.urlopen(url) as url:
 			webContent = str(url.read())
-			webContent = "<pre>" + webContent + "</pre>"
 			#webContent = str(webContent.splitlines())
 			#webContent = webContent.strip("b'")
 			#webContent = webContent[:-1]
-			#webContent = webContent.replace("\\n", " <br> ")
-			#webContent = webContent.replace("\\b", "")
+			webContent = webContent.replace("\\n", " <br> ")
+			webContent = webContent.replace("\\b", "")
 		f = open('sourcecode.html', 'w')
 		f.write(webContent)
 		f.close()
