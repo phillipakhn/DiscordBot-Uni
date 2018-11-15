b6825bd528d241e2b605100dd5e4a039737068c5
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2018-11-15 16:36:28 +0000

    Update alastair.py

--- alastair.py
+++ alastair.py
@@ -161,8 +161,7 @@ def temperature(message):
 			#webContent = str(webContent.splitlines())
 			webContent = webContent.strip("b'")
 			webContent = webContent[:-1]
-			webContent = webContent.replace("\\n", " \n ")
-			#webContent = webContent.replace("ENTER", " \n ")
+			webContent = webContent.replace("\\n", " \\n ")
 		f = open('sourcecode.html', 'w')
 		f.write(webContent)
 		f.close()
