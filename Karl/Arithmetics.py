ce64530343d3ca239671e76823bee7204ff66fa2
Karl Atkinson-Walker (walke181) <walke181@coventry.ac.uk>
2018-11-22 17:35:47 +0000

    Update Arithmetics.py

--- Karl/Arithmetics.py
+++ Karl/Arithmetics.py
@@ -90,15 +90,17 @@ async def on_ready():
 #This part of the code is not mine....
 client.run(TOKEN)
 
-async def Square(message):
+asnyc def Square(message):
     if message.content.startswith('!Square'):
         Squaremsg = str(message.content)
         Squaremsg = Squaremsg.strip('!Squaremsg ')
-	Squaremsg = int(Squaremsg)
+        Squaremsg = int(Squaremsg)
         print(Squaremsg)
         SquaredNum = int(message) * int(message)
+        return True
         await client.send_message(message.channel,"The square number for the number" + str(message) "and the square value of the number is" + str(SquaredNum))
     else:
+        return False
         await client.send_message(message.channel,"Square number has failed.")
     
 @client.event
