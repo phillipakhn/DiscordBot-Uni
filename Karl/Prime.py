b030ff21d8e17ef75367067c6c65a6a58e61d1b5
Karl Atkinson <walke181@coventry.ac.uk>
2018-11-27 23:51:22 +0000

    Discord

--- Karl/Prime.py
+++ Karl/Prime.py
@@ -5,7 +5,7 @@ TOKEN = 'NTAyMjI2MDYzNzYyMzkxMDUw.Dqk38g.KFDtyaHwMfRNK6RRUKYfpDj9L9Y'
 client = discord.Client()
 
 @client.event
-def primeN(message):
+async def primeN(message):
     if message.content.startswith('!Primes'):#This is what the user will have to type in on discord so that it can start program
         Primemsg = str(message.content)
         Primemsg = Primemsg.strip('!Primes ')
