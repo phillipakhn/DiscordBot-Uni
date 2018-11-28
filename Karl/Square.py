24409759f80240dfe9b23681101707ec86fbd85f
Karl Atkinson <walke181@coventry.ac.uk>
2018-11-28 12:19:28 +0000

    Discord

--- Karl/Square.py
+++ Karl/Square.py
@@ -1,12 +1,12 @@
 def Square(message):
-    if message.content.startswith('!SquareP'):
+    if message.content.startswith('!Square'):
         Squaremsg = str(message.content)
-        Squaremsg = Squaremsg.strip('!SquareP ')
+        Squaremsg = Squaremsg.strip('!Square ')
         Squaremsg = int(Squaremsg)
         print(Squaremsg)
         SquaredNum = int(message) * int(message)
         return (SquaredNum)
-        await client.send_message(message.channel,"The square number for the number" + str(message) "and the square value of the number is" + str(SquaredNum))
+        await client.send_message(message.channel,"The square number for the number" + str(message) + "and the square value of the number is" + str(SquaredNum))
         
 @client.event
 async def on_message(message):
