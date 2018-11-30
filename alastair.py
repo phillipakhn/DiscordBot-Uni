d0718437ac47be5f3cc8a9d4ee86bb9260e624af
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2018-11-30 19:09:12 +0000

    Update alastair.py

--- alastair.py
+++ alastair.py
@@ -71,20 +71,17 @@ def askQuestion():
 		
 '''Checks to see if the user is asking for the URL's and then displays all the stored URLs'''
 def url(message):
-	print("T0")
 	if message.content.startswith('!URL'): #If the user asks for all the URLs
 		i = 0
-		print("T1")
 		with open("url.txt",'rb') as rfp: #Load in all the stored URLs
 			url = pickle.load(rfp)
 		msg = ""
-		#for i in url:
-		#	msg = msg + "\n" + i #Adds all the URLs to a string
-		while i <= (len(url)-1): #Add All urls to a string
-			msg = msg + "\n" + url[i]
-			i = i + 1
-			print(i)
-		print("T2")
+		for i in url:
+			msg = msg + "\n" + i #Adds all the URLs to a string
+		#while i <= (len(url)-1): #Add All urls to a string
+		#	msg = msg + "\n" + url[i]
+		#	i = i + 1
+		#	print(i)
 		return msg
 	return
 	
